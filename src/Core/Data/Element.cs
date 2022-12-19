0773d24a1a8889bd34ea247390f1c6f15ace0676
Barsan1 <barsan33@gmail.com>
2022-12-19 20:38:10 +0200

    Add sample data, mirror classes

---
+++ src/Core/Data/Element.cs
@@ -0,0 +1,48 @@
+﻿namespace Core.Data
+{
+    public class Rootobject
+    {
+        public Table[] table { get; set; }
+        public Lanthanoid[] lanthanoids { get; set; }
+        public Actinoid[] actinoids { get; set; }
+    }
+
+    public class Table
+    {
+        public string wiki { get; set; }
+        public Element[] elements { get; set; }
+    }
+
+    public class Element
+    {
+        public string group { get; set; }
+        public int position { get; set; }
+        public string name { get; set; }
+        public int number { get; set; }
+        public string small { get; set; }
+        public float molar { get; set; }
+        public int[] electrons { get; set; }
+    }
+
+    public class Lanthanoid
+    {
+        public string group { get; set; }
+        public int position { get; set; }
+        public string name { get; set; }
+        public int number { get; set; }
+        public string small { get; set; }
+        public float molar { get; set; }
+        public int[] electrons { get; set; }
+    }
+
+    public class Actinoid
+    {
+        public string group { get; set; }
+        public int position { get; set; }
+        public string name { get; set; }
+        public int number { get; set; }
+        public string small { get; set; }
+        public float molar { get; set; }
+        public int[] electrons { get; set; }
+    }
+}
