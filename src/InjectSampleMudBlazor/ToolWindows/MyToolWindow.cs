7c4efb78c564b36253a75f95858d0fe28bc07f26
Barsan1 <barsan33@gmail.com>
2022-12-20 19:57:36 +0200

    Modify tool title, sample components data json file on startup

--- src/InjectSampleMudBlazor/ToolWindows/MyToolWindow.cs
+++ src/InjectSampleMudBlazor/ToolWindows/MyToolWindow.cs
@@ -1,20 +1,58 @@
-﻿using Microsoft.VisualStudio.Imaging;
+﻿using Core.Services;
+using Core.Store;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.Imaging;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using Core.Data;
 
 namespace InjectSampleMudBlazor
 {
     public class MyToolWindow : BaseToolWindow<MyToolWindow>
     {
-        public override string GetTitle(int toolWindowId) => "My Tool Window";
+        private string _cacheDataFolder;
+
+        public override string GetTitle(int toolWindowId) => Core.Constants.Extention_Name;
 
         public override Type PaneType => typeof(Pane);
 
-        public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+        public override async Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
         {
-            return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+            try
+            {
+                using MISComponentsService _componentsService = new MISComponentsService();
+
+                try
+                {
+                    if (string.IsNullOrEmpty(_cacheDataFolder))
+                    {
+                        _cacheDataFolder = Path.GetDirectoryName(typeof(Element).Assembly.Location);
+                        Directory.CreateDirectory($"{_cacheDataFolder}/cache");
+                    }
+
+                    ComponentsStore.RegisterComponentsFromCache(await _componentsService.LoadComponentsFromJsonFileAsync($"{_cacheDataFolder}/cache/componenets.json"));
+                }
+                catch (Exception ex)
+                {
+                    if (ex is FileNotFoundException)
+                    {
+                        var data = await _componentsService.GetTreeResponseAsync();
+                        ComponentsStore store = new ComponentsStore(data.tree);
+                        await _componentsService.WriteComponentsToJsonFileAsync($"{_cacheDataFolder}/cache/componenets.json", ComponentsStore.Components);
+                    }
+                    Console.WriteLine(ex);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                await VS.MessageBox.ShowWarningAsync("Inject sample package throw exception!", $"ex : {ex.Message}");
+            }
+
+            return await Task.FromResult<FrameworkElement>(new MyToolWindowControl());
         }
 
         [Guid("c1b41ddc-fd1c-4040-8063-e6e26f571e98")]
