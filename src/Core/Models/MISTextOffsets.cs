1bfd18e8dc5fb63f73cfbc3572b7402f47167f70
Barsan1 <barsan33@gmail.com>
2022-12-20 19:55:14 +0200

    Add models to control where to inject and what to inject to open document

---
+++ src/Core/Models/MISTextOffsets.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Models
+{
+    public class MISTextOffsets
+    {
+        public int SectionOffset { get; set; }
+        public int LinesAddedToSection { get; set; }
+    }
+}
---
+++ src/Core/Models/MISTextOrginaize.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Core.Models
+{
+    public class MISTextOrginaize
+    {
+        /// <summary>
+        /// usings section -> TextSections[0] <br/>
+        /// Html section -> TextSections[1] <br/>
+        /// C# section -> TextSections[2] <br/>
+        /// </summary>
+        public string[] TextSections { get; set; }
+
+        public MISTextOffsets UsingsSection { get; set; } = new MISTextOffsets();
+        public MISTextOffsets HtmlSection { get; set; } = new MISTextOffsets();
+        public MISTextOffsets CodeSection { get; set; } = new MISTextOffsets();
+
+
+        public MISTextOrginaize(int usingsSectionOffset, int htmlSectionOffset, int codeSectionOffset)
+        {
+            UsingsSection.SectionOffset = usingsSectionOffset;
+            HtmlSection.SectionOffset = htmlSectionOffset;
+            CodeSection.SectionOffset = codeSectionOffset;
+        }
+    }
+
+}
