625e30144090ed4291de08e37c0ff1b55936ba04
Barsan1 <barsan33@gmail.com>
2022-12-20 19:58:29 +0200

    Modify tool window xaml and c#

--- src/InjectSampleMudBlazor/ToolWindows/MyToolWindowControl.xaml.cs
+++ src/InjectSampleMudBlazor/ToolWindows/MyToolWindowControl.xaml.cs
@@ -1,18 +1,46 @@
-﻿using System.Windows;
+﻿using Core.Models;
+using Core.Store;
+using InjectSampleMudBlazor.Commands;
+using System.Linq;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace InjectSampleMudBlazor
 {
     public partial class MyToolWindowControl : UserControl
     {
+        private ICommand _sampleComponentCommand = new SampleComponentCommand();
+
         public MyToolWindowControl()
         {
             InitializeComponent();
+            AddSampleButtons();
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void AddSampleButtons()
         {
-            VS.MessageBox.Show("InjectSampleMudBlazor", "Button clicked");
+            if (ComponentsStore.Components.Any())
+            {
+                foreach (var father in ComponentsStore.Components)
+                {
+                    foreach (var sampleSon in father.SampleComponents)
+                    {
+                        stackPanel.Children.Add(new Button()
+                        {
+                            Content = sampleSon.Name,
+                            HorizontalContentAlignment = System.Windows.HorizontalAlignment.Left,
+                            Background = Brushes.Transparent,
+                            Foreground = Brushes.White,
+                            Command = _sampleComponentCommand,
+                            CommandParameter = new RawComponentDetails(father.ComponentsArea, sampleSon.Name)
+                        });  
+                    }
+                }
+            }
+            
+            // Clear components store components
+            ComponentsStore.Components.Clear();
         }
     }
 }
