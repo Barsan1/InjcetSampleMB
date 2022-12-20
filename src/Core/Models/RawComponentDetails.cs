9ffe8df6717b6a1ef2805b9957a2f2a726e4b583
Barsan1 <barsan33@gmail.com>
2022-12-20 19:54:42 +0200

    Add model to command parmeter for inject sample button

---
+++ src/Core/Models/RawComponentDetails.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Core.Models
+{
+    public class RawComponentDetails
+    {
+        public string Area { get; set; }
+        public string SampleName { get; set; }
+
+        public RawComponentDetails(string area, string sampleName)
+        {
+            Area = area;
+            SampleName = sampleName;
+        }
+    }
+}
