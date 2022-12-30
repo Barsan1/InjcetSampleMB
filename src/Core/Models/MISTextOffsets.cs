6cbb42f82321ac19b9fdbac66e20547dacef333a
Barsan1 <barsan33@gmail.com>
2022-12-30 18:47:59 +0200

    Add Comments

--- src/Core/Models/MISTextOffsets.cs
+++ src/Core/Models/MISTextOffsets.cs
@@ -2,7 +2,14 @@
 {
     public class MISTextOffsets
     {
+        /// <summary>
+        /// Where is the section in razor component start
+        /// </summary>
         public int SectionOffset { get; set; }
+
+        /// <summary>
+        /// Number of lines the section need to add after inject the sample
+        /// </summary>
         public int LinesAddedToSection { get; set; }
     }
 }
