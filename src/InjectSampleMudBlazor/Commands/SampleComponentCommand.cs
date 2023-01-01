576010db43ee305ad425d46b3f0d08850c3094e9
Barsan1 <barsan33@gmail.com>
2023-01-01 13:55:31 +0200

    Modify command to insert the sample

--- src/InjectSampleMudBlazor/Commands/SampleComponentCommand.cs
+++ src/InjectSampleMudBlazor/Commands/SampleComponentCommand.cs
@@ -1,8 +1,8 @@
-﻿using Core;
-using Core.Models;
+﻿using Core.Models;
 using Core.Services;
 using Microsoft.VisualStudio.Text;
 using System.Windows.Input;
+using System.Diagnostics;
 
 namespace InjectSampleMudBlazor.Commands
 {
@@ -23,10 +23,11 @@ namespace InjectSampleMudBlazor.Commands
             // Check if there is document open
             if (docView?.TextView == null || !(bool)(docView?.FilePath.EndsWith(".razor")))
             {
-                await VS.MessageBox.ShowErrorAsync($"{Constants.Extention_Name} Provide an Error", "Please open a razor component document to insert the sample");
-                return; 
+                await VS.MessageBox.ShowErrorAsync($"{Core.Constants.Extention_Name} Provide an Error", "Please open a razor component document to insert the sample");
+                return;
             }
 
+            // Load component from github
             using (MISComponentsService cs = new MISComponentsService())
             {
                 rawSample = await cs.LoadSampleComponentAsync(details.Area, details.SampleName);
@@ -34,18 +35,44 @@ namespace InjectSampleMudBlazor.Commands
 
             MISDocumentTextService misTextService = new MISDocumentTextService();
 
-            // Get offsets from current razor document view
+            // Get offsets and text sections from current razor document view (Opened in visual studio)
             var sectionsData = await misTextService.TextSectionDivderAsync(docView.TextView.TextBuffer.CurrentSnapshot.GetText());
 
+            MISExcludeTextOptions options = new MISExcludeTextOptions(sectionsData.IsCodeSectionExist);
+            sectionsData.MISExcludeTextOptions = options;
 
             await misTextService.OrginazeTextInputAsync(rawSample, sectionsData);
-            
+
             // Inserts text at the caret (HTML)
             docView.TextBuffer?.Insert(docView.TextView.Caret.Position.BufferPosition, sectionsData.TextSections[1]);
 
+            // Debug
+            Debug.WriteLine(docView.TextView.TextBuffer.CurrentSnapshot.GetText());
+
+            // Check if need to add usings in razor component
+            if (sectionsData.UsingsSection.LinesAddedToSection > 1)
+                docView.TextBuffer?.Insert(0, sectionsData.TextSections[0]);
+
             // Inserts text (C#)
-            ITextSnapshotLine secondLine = docView.TextBuffer.CurrentSnapshot.GetLineFromLineNumber(sectionsData.CodeSection.SectionOffset + sectionsData.HtmlSection.LinesAddedToSection);
-            docView.TextBuffer?.Insert(secondLine.Start, sectionsData.TextSections[2]);
+            if (sectionsData.CodeSection.LinesAddedToSection != 0)
+            {
+                ITextSnapshotLine secondLine;
+                // TODO: Create logic method to orginaze the c sharp code section (Own method)
+                if (sectionsData.IsCodeSectionExist)
+                {
+                    await misTextService.CodeSectionOrginazeAsync(sectionsData);
+                    
+                    // Orginal place + html lines added + usings lines added - 1 + 2 (Actual comments)
+                    int calculatelineToAdd = sectionsData.CodeSection.SectionOffset + sectionsData.HtmlSection.LinesAddedToSection + sectionsData.UsingsSection.LinesAddedToSection + 2;
+                    secondLine = docView.TextBuffer.CurrentSnapshot.GetLineFromLineNumber(calculatelineToAdd);
+                }
+                else
+                {
+                    secondLine = docView.TextBuffer.CurrentSnapshot.GetLineFromLineNumber(docView.TextBuffer.CurrentSnapshot.LineCount);
+                }
+
+                docView.TextBuffer?.Insert(secondLine.Start, sectionsData.TextSections[2]);
+            }
         }
     }
 }
