8be28b6a4245c556fa9c859a744c2e2d2ad0d90b
Barsan1 <barsan33@gmail.com>
2023-01-24 18:41:38 +0200

    Modify ui elements, add area in drop down

--- src/InjectSampleMudBlazor/ToolWindows/MyToolWindowControl.xaml.cs
+++ src/InjectSampleMudBlazor/ToolWindows/MyToolWindowControl.xaml.cs
@@ -1,44 +1,53 @@
 ﻿using Core.Models;
 using Core.Store;
 using InjectSampleMudBlazor.Commands;
-using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 
 namespace InjectSampleMudBlazor
 {
-    public partial class MyToolWindowControl : UserControl
+    public partial class MyToolWindowControl : UserControl, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
         private ICommand _sampleComponentCommand = new SampleComponentCommand();
 
         public MyToolWindowControl()
         {
             InitializeComponent();
-            AddSampleButtons();
+            DataContext = this;
         }
 
-        private void AddSampleButtons()
+        private string _currentArea = "0";
+        public string CurrentArea
         {
-            if (ComponentsStore.Components.Any())
+            get { return _currentArea; }
+            set
             {
-                foreach (var father in ComponentsStore.Components)
-                {
-                    foreach (var sampleSon in father.SampleComponents)
-                    {
-                        stackPanel.Children.Add(new Button()
-                        {
-                            Content = sampleSon.Name,
-                            HorizontalContentAlignment = System.Windows.HorizontalAlignment.Left,
-                            Command = _sampleComponentCommand,
-                            CommandParameter = new RawComponentDetails(father.ComponentsArea, sampleSon.Name)
-                        });  
-                    }
-                }
+                _currentArea = value;
+                Exampels = null;
             }
-            
-            // Clear components store components
-            ComponentsStore.Components.Clear();
+        }
+
+        private List<ComponentSon> _exampels = ComponentsStore.Components[0].SampleComponents;
+        public List<ComponentSon> Exampels
+        {
+            get { return _exampels; }
+            set
+            {
+                _exampels = ComponentsStore.Components[Convert.ToInt32(_currentArea)].SampleComponents;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Exampels)));
+            }
+        }
+
+        private void list_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var x = (sender as System.Windows.Controls.ListView).SelectedItem as ComponentSon;
+            if (x != null)
+            {
+                _sampleComponentCommand.Execute(new RawComponentDetails(ComponentsStore.Components[Convert.ToInt32(_currentArea)].ComponentsArea, x.Name));  
+            }
         }
     }
 }
